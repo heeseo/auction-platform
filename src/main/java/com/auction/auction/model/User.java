82e25f773ac54d3ee733560d1476adecbb21376a
Heekwon Seo <seoheekwon@gmail.com>
2025-08-19 23:27:58 +0100

    feat: implement UserDetails and CredentialsContainer in User entity for security

--- src/main/java/com/auction/auction/model/User.java
+++ src/main/java/com/auction/auction/model/User.java
@@ -2,16 +2,22 @@ package com.auction.auction.model;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.CredentialsContainer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+@Table(name = "users")
+public class User implements UserDetails, CredentialsContainer {
 
     @Id @GeneratedValue
     @Column(name = "user_id")
@@ -26,6 +32,13 @@ public class User {
     @OneToMany(mappedBy = "bidder", cascade = CascadeType.ALL)
     private List<Bid> bids = new ArrayList<>();
 
+    @Builder
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
     public void addItemForSale(Item item) {
         this.itemsForSale.add(item);
     }
@@ -33,4 +46,15 @@ public class User {
     public void addBid(Bid bid) {
         this.bids.add(bid);
     }
+
+    @Override
+    public void eraseCredentials() {
+        this.password = null; // Clear password for security
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "ROLE_USER");
+    }
+
 }
