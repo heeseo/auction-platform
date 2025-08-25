dc20699ca200053eebebd9005a46f53c518d6b8e
Heekwon Seo <seoheekwon@gmail.com>
2025-08-25 02:06:59 +0100

    feat: add endpoint to retrieve current user information

--- src/main/java/com/auction/auction/controller/AuthController.java
+++ src/main/java/com/auction/auction/controller/AuthController.java
@@ -3,13 +3,13 @@ package com.auction.auction.controller;
 import com.auction.auction.dto.UserLoginRequest;
 import com.auction.auction.dto.UserRegistrationRequest;
 import com.auction.auction.dto.UserResponse;
+import com.auction.auction.model.User;
 import com.auction.auction.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +23,11 @@ public class AuthController {
     public UserResponse registerUser(@RequestBody UserRegistrationRequest request) {
         log.info("Registering new user: {}", request.getUsername());
         return userService.registerUser(request);
+    }
+
+    @GetMapping("/users/me")
+    public ResponseEntity<UserResponse> getCurrentUser(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(new UserResponse(user.getId(), user.getUsername(), user.getEmail()));
     }
 
 }
